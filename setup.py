438edf1e7896b188327e9ab0767669c8e5919186
bit-shashank <shashankcs083@gmail.com>
2020-08-09 23:41:16 +0530

    Changed Comments

--- setup.py
+++ setup.py
@@ -7,27 +7,27 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name = 'tinn',         # How you named your package folder (MyLib)
-    packages = ['tinn'],   # Chose the same as "name"
-    version = '2.01',      # Start with a small number and increase it with every change you make
-    license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description = 'A light weight simple, multi layer ,feedforward neural network library',   # Give a short description about your library
+    name = 'tinn',         
+    packages = ['tinn'],   
+    version = '2.01',      
+    license='MIT',     
+    description = 'A light weight simple, multi layer ,feedforward neural network library',   
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author = 'Shashank Sahu',                   # Type in your name
-    author_email = 'shashankcs083@gmail.com',      # Type in your E-Mail
-    url = 'https://github.com/bit-shashank/tinn',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/bit-shashank/tinn/archive/v0.2-alpha.tar.gz',    # I explain this later on
-    keywords = ['Neural', 'Deep', 'Learning' ,'Machine','Network'],   # Keywords that define your package best
-    install_requires=[            # I get to this in a second
+    author = 'Shashank Sahu',        
+    author_email = 'shashankcs083@gmail.com',    
+    url = 'https://github.com/bit-shashank/tinn', 
+    download_url = 'https://github.com/bit-shashank/tinn/archive/v0.2-alpha.tar.gz',  
+    keywords = ['Neural', 'Deep', 'Learning' ,'Machine','Network'],   
+    install_requires=[          
             'numpy',
         ],
     classifiers=[
-        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Development Status :: 3 - Alpha',     
+        'Intended Audience :: Developers',      
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'License :: OSI Approved :: MIT License',   
+        'Programming Language :: Python :: 3',     
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
